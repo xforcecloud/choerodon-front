d0eaf222e69fe768554ad23161baa4c2cf1ff81d
gaohuikai <gaohuikai@xforceplus.com>
2018-12-06 16:50:01 +0800

    fat 0.11.3 update boot version

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
 const config = {
   local: true, //是否为本地开发
   clientId: 'localhost', // 必须填入响应的客户端（本地开发）
-  titlename: 'Choerodon | 企业数字化服务平台', //项目页面的title名称
+  titlename: 'XforceCloud | 上海云励信息科技', //项目页面的title名称
   favicon: 'favicon.ico', //项目页面的icon图片名称
   fileServer: 'http://minio.choerodon.example.com',
   theme: {
-    'primary-color': '#3F51B5',
+    'primary-color': '#00896C',
   },
   cookieServer: '', //子域名token共享
   server: 'http://api.choerodon.example.com',
